d9a751e7585fbf6f5acf518135ff6c4b4d9fa0f6
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-11 23:31:34 +0600

    first commit

---
+++ js/error.js
@@ -0,0 +1,49 @@
+//* 1. syntaxError
+// console.log(5;
+/* function()){
+
+} */
+// Let x = 5;
+// cont y = 10;
+
+//* 2. typeError
+// let address;
+// console.log(address)
+// console.log(address.length);
+
+const people = 5;
+people();
+
+/* const persons = {
+    name: 'takbir'
+}
+console.log(persons) */
+// persons.map(a => console.log(a))
+
+
+//* 3. referenceError
+//? kono 1ta kisu khujtesi kintu oita exist korena or scope er baire ase. taile referenceError dibe
+
+/* function add(a,b){
+    const result = a + b;
+    return result;
+}
+console.log(result);
+ */
+/* console.log(test);
+const test = 5; */
+//? var hoisted, tai ReferenceError dibe na 
+/* test2 = 5;
+console.log(test2);
+var test2; */
+console.log(start)
+const start = 10;
+
+
+
+
+
+
+//* rangeError
+/* const arr = [90,88]
+arr.length=90**99 */
